7fd3f652fc3ae7abb20a919c19feac740dba66f7
Sini Arkko <83418050+SiniCode@users.noreply.github.com>
2024-05-28 18:49:35 +0300

    Take prettier into use

--- part7/blog-list/backend/tests/users_api.test.js
+++ part7/blog-list/backend/tests/users_api.test.js
@@ -9,93 +9,79 @@ const assert = require('assert')
 const User = require('../models/user')
 
 describe('users', () => {
-  beforeEach(async () => {
-    await User.deleteMany({})
-  })
+	beforeEach(async () => {
+		await User.deleteMany({})
+	})
 
-  test('a valid user can be added', async () => {
-    const newUser = {
-      username: "newuser",
-      name: "New User",
-      password: "password"
-    }
+	test('a valid user can be added', async () => {
+		const newUser = {
+			username: 'newuser',
+			name: 'New User',
+			password: 'password',
+		}
 
-    const usersAtStart = await helper.usersInDb()
+		const usersAtStart = await helper.usersInDb()
 
-    const response = await api
-      .post('/api/users')
-      .send(newUser)
-      .expect(201)
+		const response = await api.post('/api/users').send(newUser).expect(201)
 
-    assert.strictEqual(response.body.username, newUser.username)
-    assert.strictEqual(response.body.name, newUser.name)
+		assert.strictEqual(response.body.username, newUser.username)
+		assert.strictEqual(response.body.name, newUser.name)
 
-    const usersAtEnd = await helper.usersInDb()
+		const usersAtEnd = await helper.usersInDb()
 
-    assert.strictEqual(usersAtEnd.length, usersAtStart.length + 1)
-  })
+		assert.strictEqual(usersAtEnd.length, usersAtStart.length + 1)
+	})
 
-  test('user without username is not added', async () => {
-    const newUser = {
-      name: "New User",
-      password: "password"
-    }
+	test('user without username is not added', async () => {
+		const newUser = {
+			name: 'New User',
+			password: 'password',
+		}
 
-    const usersAtStart = await helper.usersInDb()
+		const usersAtStart = await helper.usersInDb()
 
-    const result = await api
-      .post('/api/users')
-      .send(newUser)
-      .expect(400)
+		const result = await api.post('/api/users').send(newUser).expect(400)
 
-    const usersAtEnd = await helper.usersInDb()
+		const usersAtEnd = await helper.usersInDb()
 
-    assert.strictEqual(usersAtEnd.length, usersAtStart.length)
-  })
+		assert.strictEqual(usersAtEnd.length, usersAtStart.length)
+	})
 
-  test('user without password is not added', async () => {
-    const newUser = {
-      username: "newuser",
-      name: "New User"
-    }
+	test('user without password is not added', async () => {
+		const newUser = {
+			username: 'newuser',
+			name: 'New User',
+		}
 
-    const usersAtStart = await helper.usersInDb()
+		const usersAtStart = await helper.usersInDb()
 
-    const result = await api
-      .post('/api/users')
-      .send(newUser)
-      .expect(400)
+		const result = await api.post('/api/users').send(newUser).expect(400)
 
-    const usersAtEnd = await helper.usersInDb()
+		const usersAtEnd = await helper.usersInDb()
 
-    assert.strictEqual(result.body.error, 'password missing or too short')
-    assert.strictEqual(usersAtEnd.length, usersAtStart.length)
-  })
+		assert.strictEqual(result.body.error, 'password missing or too short')
+		assert.strictEqual(usersAtEnd.length, usersAtStart.length)
+	})
 
-  test.only('same username can not be addwd twice' , async () => {
-    const newUser = {
-      username: "newuser",
-      name: "New User",
-      password: "password"
-    }
-  
-    await api
-      .post('/api/users')
-      .send(newUser)
-  
-    const usersAtStart = await helper.usersInDb()
+	test.only('same username can not be addwd twice', async () => {
+		const newUser = {
+			username: 'newuser',
+			name: 'New User',
+			password: 'password',
+		}
 
-    await api
-      .post('/api/users')
-      .send(newUser)
-      .expect(400)
-  
-    const usersAtEnd = await helper.usersInDb()
-  
-    assert.strictEqual(usersAtEnd.length, usersAtStart.length)
-  })
+		await api.post('/api/users').send(newUser)
 
-  after(() => {
-    mongoose.connection.close()
-  })
+		const usersAtStart = await helper.usersInDb()
+
+		await api.post('/api/users').send(newUser).expect(400)
+
+		const usersAtEnd = await helper.usersInDb()
+
+		assert.strictEqual(usersAtEnd.length, usersAtStart.length)
+	})
+
+	after(() => {
+		mongoose.connection.close()
+	})
 })
