24ee9e5f90345f77d371d219c3ff84c58b6a8d1f
Sini Arkko <83418050+SiniCode@users.noreply.github.com>
2024-07-13 19:50:33 +0300

    Create endpoint for fetching all patients without ssn

--- part9/patientor-backend/src/index.ts
+++ part9/patientor-backend/src/index.ts
@@ -1,5 +1,7 @@
 import express from 'express';
 import diagnosisRouter from './routes/diagnoses';
+import patientRouter from './routes/patients';
+
 const app = express();
 import cors from 'cors';
 app.use(cors());
@@ -13,6 +15,7 @@ app.get('/api/ping', (_req, res) => {
 });
 
 app.use('/api/diagnoses', diagnosisRouter);
+app.use('/api/patients', patientRouter);
 
 app.listen(PORT, () => {
 	console.log(`Server running on port ${PORT}`);
