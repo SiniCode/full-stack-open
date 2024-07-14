3eb44ef7c53a91099e55a91fc2323ef0ae54b0b7
Sini Arkko <83418050+SiniCode@users.noreply.github.com>
2024-07-15 00:07:15 +0300

    Add form for adding new diary entries

--- part9/flight-diary-frontend/src/App.tsx
+++ part9/flight-diary-frontend/src/App.tsx
@@ -1,22 +1,93 @@
 import { useState, useEffect } from 'react';
 import EntryList from './components/EntryList';
-import { NonSensitiveDiaryEntry } from './types';
-import { getDiaries } from './services/diaryService';
+import {
+	NonSensitiveDiaryEntry,
+	NewDiaryEntry,
+	Visibility,
+	Weather,
+} from './types';
+import { getEntries, addEntry } from './services/diaryService';
 
 function App() {
 	const [diaryEntries, setDiaryEntries] = useState<NonSensitiveDiaryEntry[]>(
 		[]
 	);
+	const [date, setDate] = useState('');
+	const [visibility, setVisibility] = useState('');
+	const [weather, setWeather] = useState('');
+	const [comment, setComment] = useState('');
 
 	useEffect(() => {
-		getDiaries().then((data) => {
+		getEntries().then((data) => {
 			setDiaryEntries(data);
 		});
 	}, []);
 
+	const handleSubmit = (event: React.SyntheticEvent) => {
+		event.preventDefault();
+		const newEntry: NewDiaryEntry = {
+			date,
+			visibility: visibility as Visibility,
+			weather: weather as Weather,
+			comment,
+		};
+		addEntry(newEntry).then((data) => {
+			console.log(data);
+			setDiaryEntries(
+				diaryEntries.concat({
+					id: data.id,
+					date: data.date,
+					visibility: data.visibility,
+					weather: data.weather,
+				})
+			);
+		});
+		setDate('');
+		setVisibility('');
+		setWeather('');
+		setComment('');
+	};
+
 	return (
 		<>
-			<h1>Flight Diaries</h1>
+			<h1>Flight Diary</h1>
+			<div className='component'>
+				<h2>Add new entry</h2>
+				<form onSubmit={handleSubmit}>
+					<div>
+						Date:
+						<input
+							value={date}
+							placeholder='YYYY-MM-DD'
+							onChange={(event) => setDate(event.target.value)}
+						/>
+					</div>
+					<div>
+						Visibility:
+						<input
+							value={visibility}
+							placeholder='great/good/ok/poor'
+							onChange={(event) => setVisibility(event.target.value)}
+						/>
+					</div>
+					<div>
+						Weather:
+						<input
+							value={weather}
+							placeholder='sunny/rainy/cloudy/stormy/windy'
+							onChange={(event) => setWeather(event.target.value)}
+						/>
+					</div>
+					<div>
+						Comment:
+						<textarea
+							value={comment}
+							onChange={(event) => setComment(event.target.value)}
+						/>
+					</div>
+					<button type='submit'>Add to diary</button>
+				</form>
+			</div>
 			<EntryList entries={diaryEntries} />
 		</>
 	);
--- part9/flight-diary-frontend/src/components/EntryList.tsx
+++ part9/flight-diary-frontend/src/components/EntryList.tsx
@@ -3,9 +3,10 @@ import { EntryListProps, NonSensitiveDiaryEntry } from '../types';
 
 const EntryList = (props: EntryListProps) => {
 	return (
-		<div className='entries'>
-			{props.entries.map((entry: NonSensitiveDiaryEntry, i: number) => (
-				<DiaryEntry key={`entry${i}`} entry={entry} />
+		<div className='component'>
+			<h2>Diary Entries</h2>
+			{props.entries.map((entry: NonSensitiveDiaryEntry) => (
+				<DiaryEntry key={entry.id} entry={entry} />
 			))}
 		</div>
 	);
